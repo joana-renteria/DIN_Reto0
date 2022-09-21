d83c7e36a925d2651708ee266484dfe56508643a
joana-renteria <joana.rente6@tuta.io>
2022-09-21 08:43:48 +0200

    implemented both views and created the controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,15 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
-/**
- *
- * @author 2dam
- */
 public class Controller {
-    
-    
+
+	public Controller() {
+	}
+
+	public void run(View view, Model model) {
+		String greeting = model.getGreeting();
+		view.showGreeting(greeting);
+	}
+
 }
