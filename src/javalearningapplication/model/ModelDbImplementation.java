af310c690832bfd3fc37d36aa2e820ada0924db4
EnekoRuiz <enekoerre@gmail.com>
2022-09-29 20:40:36 +0200

    Pulled main from devJavaDoc

--- src/javalearningapplication/model/ModelDbImplementation.java
+++ src/javalearningapplication/model/ModelDbImplementation.java
@@ -1,16 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javalearningapplication.model;
 
 /**
  *
  * @author 2dam
+ * This class accesses the database
  */
 public class ModelDbImplementation implements Model{
 
+    /**
+     * 
+     * Enters the database and returns the message 
+     */
     @Override
     public String getGreeting() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
